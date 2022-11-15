e2a36fc86df56de5a9cd73d292a9a533b5412066
scydive <aadne1999@live.no>
2022-11-15 15:34:14 +0100

    testing docker

--- mysite/manage.py
+++ mysite/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks. b"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
